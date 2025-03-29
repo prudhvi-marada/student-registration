eff60897a38ddbc87dedbaa72df2b6bccee750a8
prudhvi-marada <maradaprudhvi@gmail.com>
2025-03-29 12:17:53 +0530

    Third  commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ The web page is live at my git hub, link given below.
+ 
+ https://prudhvi-marada.github.io/student-registration/ 
+
--- index.js
+++ index.js
@@ -103,3 +103,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     renderStudents();  //displaying student details when page loads 
 });
+
+
+
+
